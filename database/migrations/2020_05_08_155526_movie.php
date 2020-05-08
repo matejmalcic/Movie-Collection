09f4258c03aa2c9697c37e645f5ff2b149883bc6
Matej Malcic <matej.malcic3@gmail.com>
2020-05-08 19:17:18 +0200

    routes

---
+++ database/migrations/2020_05_08_155526_movie.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Movie extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movie', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->integer('genre_id')->index();
+            $table->integer('year');
+            $table->integer('duration');
+            $table->string('image');
+        }); 
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movie');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => ['web']], function () {
+
+    Route::auth();
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::get('/movies', 'MovieController@index');
+    Route::post('/movies', 'MovieController@store');
+    Route::delete('/movies/{movie_id}', 'MovieController@destroy');
+
+    Route::get('/movies/edit/{movie_id}', 'MovieController@edit');
+    Route::get('/movies/{movie_id}', 'MovieController@update');
+  
 });
+
+Route::get('/home', 'HomeController@index')->name('home');
